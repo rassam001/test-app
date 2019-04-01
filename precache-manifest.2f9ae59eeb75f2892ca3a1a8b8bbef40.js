b28359117f4164538c61aa86928fd41f33aa9533
rassam001 <aniss.elgharnit@devugees.org>
2019-04-01 12:38:26 +0200

    Updates

---
+++ precache-manifest.2f9ae59eeb75f2892ca3a1a8b8bbef40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7318f3809038590af7ec",
+    "url": "/test-app/static/js/runtime~main.e9adfd5a.js"
+  },
+  {
+    "revision": "c7b49b259a5af8f240f5",
+    "url": "/test-app/static/js/main.069061ee.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/test-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c7b49b259a5af8f240f5",
+    "url": "/test-app/static/css/main.15b971ac.chunk.css"
+  },
+  {
+    "revision": "c4c6e3501ef6b40e4795a5da342a5e51",
+    "url": "/test-app/index.html"
+  }
+];
